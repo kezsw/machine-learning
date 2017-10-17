c594ecc72409a695a7a1ce3a294414e191c311ca
Aidan Scannell <as12528@my.bristol.ac.uk>
2017-10-17 22:10:54 +0100

    Calculated and plotted prior Switched x=[x, 1] vector to x=[1, x]

--- linear-regression-laura.py
+++ linear-regression-laura.py
@@ -1,7 +1,11 @@
-import pylab as pb
+############################################
+# Linear Regression
+#
+# Author: Aidan Scannell
+# Date: October 2017
+############################################
+
 import numpy as np
-from math import pi
-from scipy.spatial.distance import cdist
 from scipy.stats import norm, multivariate_normal
 import matplotlib.pyplot as plt
 from numpy.linalg import inv
@@ -11,26 +15,32 @@ mu = 0
 sigma = 0.3
 W = np.array([-1.3,0.5]) # [W1 W0]
 W = W.reshape(-1,2)
-W = W.T
 # cov=t*np.eye(W.shape[1])
 
 # covariance
-t = 0.8
+t = 0.1
 
 # generate data
 x = np.ones([2,201])
-x[0,:] = np.linspace(-1,1,201)
+x[1,:] = np.linspace(-1,1,201)
+# x = np.linspace(-1,1,201)
+# x = x.reshape(-1,1)
 
+# calculate prior
 prior_w = multivariate_normal(mean=[W.item(0),W.item(1)],cov=[[t,0],[0,t]]).pdf(x.T)
 prior_w = prior_w.reshape(-1,1)
 
-plt.plot(x[0].T,prior_w)
-plt.show()
+error = norm(mu,sigma).pdf(x)
+Y = W.T * x + error
 
-# # error = np.random.normal(0,0.1,x.shape) # noise
-# error = norm(mu,sigma).pdf(x)
-#
-# Y = np.ones(x.shape)*W[0]* x + W[1] + error
+# create figure
+fig = plt.figure(figsize=(10,5))
+ax = fig.add_subplot(111)
+ax.set_xlabel('$w$')
+ax.set_ylabel('$p(w|x,y)$')
+
+# plot prior
+ax.plot(x.T,prior_w,'b')
 
 # posterior
 # def posterior(Y):
@@ -40,18 +50,8 @@ plt.show()
 #     j = j + 1
 #     return norm(m_n,s_n_inv).pdf(x)
 
-# # create figure
-# fig = plt.figure(figsize=(10,5))
-# ax = fig.add_subplot(111)
-# ax.set_xlabel('$w$')
-# ax.set_ylabel('$p(w|x,y)$')
-#
-# # plot prior
-# ax.plot(x,prior_w,'b')
-#
-#
-# m_n = 1/sigma * inv(1/sigma * x.T * x + t*np.identity(x.shape[0])) * x.T * Y
-# s_n_inv = 1/sigma * x.T * x + t
+# m_n = 1/sigma * (inv(1/sigma * x.T.dot(x) + t*np.identity(x.shape[1]))).dot(x.T).dot(Y[:1])
+# s_n_inv = 1/sigma * (x.T).dot(x) + t*np.identity(x.shape[1])
 # posterior_w = norm(m_n,s_n_inv).pdf(x)
 #
 # ax.plot(x,posterior_w[:,100],'r')
