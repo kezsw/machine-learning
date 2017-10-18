615aab72793adf28e696b34c0fcc2dc656f705e1
lauralikespi <laura.gemmell012@gmail.com>
2017-10-18 06:53:28 +0100

    Updated Gaussian Process

--- gaussian_process.py
+++ gaussian_process.py
@@ -12,14 +12,22 @@ x = x.reshape(-1,1)
 
 mu = np.zeros(x.shape)
 K = np.exp(-cdist(x,x)**2/5)
-var = 0.5
-sigma = math.sqrt(var)
-#error = norm.pdf(X, loc=mu, scale=sigma)
 f = np.random.multivariate_normal(mu.flatten(),K,10)
 
-#y = np.sin(x) + error
+
+x2 = np.linspace(-pi,pi,7)
+x2= x.reshape(-1,1)
+
+mu2 = np.zeros(x2.shape)
+K2 = np.exp(-cdist(x2,x2)**2/5)
+mu1 = 0
+var3 = 0.5
+sigma3 = math.sqrt(var3)
+error norm.pdf(X, loc=mu, scale=sigma1)
+y = np.sin(x2) + error
+y= y.reshape(-1,1)
 
 #y2 = np.sin(X2)
 
-plt.plot(x,f.T)
+plt.plot(x2,y)
 plt.show()
